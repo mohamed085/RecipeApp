123e03d44a9648f84f8f968d25bc1c08df2af14b
mohamed085 <mohamed085555@gmail.com>
2022-01-19 17:18:37 +0200

    Create repositories

---
+++ src/main/java/com/recipe/repositories/RecipeRepository.java
@@ -0,0 +1,9 @@
+package com.recipe.repositories;
+
+import com.recipe.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+}
